22c7e1216aa3f76ba66e098712a86bf67fd89bba
code004Accepted <code004accepted@gmail.com>
2022-03-29 22:05:39 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -16,10 +16,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm789531287-210b470971efb1b9146a0c53',
+    'm783825758-99570f0039c8792809ba8a0b',
+    'm789531289-6f3a565d42f27338f6ce608d',
   ],
 
   // 是否显示监测站点的链接
